9cddeade653a0479759611baf4674ce5753f7456
Foneom <mugoloka@gmail.com>
2020-05-17 14:44:38 +0300

    Убывающий массив из чисел, которые делятся 3

--- src/ru/foneom/arrayFilling/FillArrayNegativeNumbers.java
+++ src/ru/foneom/arrayFilling/FillArrayNegativeNumbers.java
@@ -4,7 +4,11 @@ package ru.foneom.arrayFilling;
  * Сlass fills the array with negative numbers
  */
 public class FillArrayNegativeNumbers {
-
+    /**
+     * This mthod fills the array with negative numbers divide by 3
+     * @param array
+     * @return
+     */
     public static int[] fillNegativeNumbersDivide3(int[] array) {
             int result = -1;
             for (int i = 0; i < array.length; i++,result--) {
